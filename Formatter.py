236a3320578ba540acbb6cd9a3e6f68f986a6637
Stephen <sfilios@users.noreply.github.com>
2016-08-05 13:43:44 -0700

    Create Formatter.py

---
+++ Formatter.py
@@ -0,0 +1,61 @@
+
+#date value adjusting script
+
+from sys import argv
+
+script, birding_data = argv
+
+for data in open(birding_data):
+
+#Check if the first character is a number
+	if float(string[0])=TRUE:
+	#If the first character is a number, then look at the second character
+	#if the second character is a number, then the day is those two numbers
+			if float(string[1])=TRUE :
+				day=str(string[0])+str(string[1])
+			#if the second character is a number, look at the third character.
+			#if the third character is a "-" then look at the fourth, fifth and sixth
+			#characters, and assign the month
+				if string[3:5]="Jan" then month=1
+				else if string[3:5]="Feb" then month=2
+				else if string[3:5]="Mar" then month=3
+				else if string[3:5]="Apr" then month=4
+		#otherwise, see if the second character is a "-"	
+			else if string[1]="-"
+				then 
+			#if it is, then look at the second, third, and fourth characters.
+			#Depending on what they are, assign a month value 
+				if string[1:3]="Jan" then month=1
+				else if string[1:3]="Feb" then month=2
+				else if string[1:3]="Mar" then month=3
+				else if string[1:3]="Apr" then month=4
+
+		else day=string[0]
+	
+	
+	if float(string[0])=TRUE and string[1]="	
+		
+		
+		
+		
+is the next character a '/'?
+If so, do nothing.
+
+If not look at the  , is the next character a number?
+	if so, 
+		
+	else: 	day=string[0]
+	
+	
+	the next character is nothing, but if the string following is Jan
+		month=1
+	if it's "Feb" then month=2
+	if it's "Mar" then month=3
+	if it's "Apr" then month=4
+	else:
+		
+	otherwise, first number =day
+
+print day + "/" + month + "/" + "2016"
+
+	
